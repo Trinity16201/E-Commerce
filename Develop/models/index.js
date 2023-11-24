a698be3450c1abd6b160f58ba60e01fb85c36ebe
Sarah Leder <Trinity16201@gmail.com>
2023-11-24 18:31:09 -0500

    added connections for each model in the index js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+  foreignkey: 'product_id',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignkey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+through: 'product_tag'
+})
 module.exports = {
   Product,
   Category,
