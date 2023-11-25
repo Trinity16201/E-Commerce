ef09dca7035d307ba1e85692fd0e3adc2e84c6b1
Sarah Leder <Trinity16201@gmail.com>
2023-11-24 20:38:29 -0500

    added operations to all route files

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await Product.findAll({ include: [{ model: Category, through: Tag, as: 'product_tag'}]})
+  res.status(200).json(productData);
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, through: Tag, as: 'product_id'}]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product Id found'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -45,7 +66,6 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
